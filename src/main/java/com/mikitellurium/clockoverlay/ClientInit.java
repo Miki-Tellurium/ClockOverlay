85d0caec2f3d47f19fde56619032b34fd802f82f
Miki_Tellurium <michelegamer14@gmail.com>
2024-06-19 18:38:32 +0200

    Renamed class

--- src/main/java/com/mikitellurium/clockoverlay/ClientInit.java
+++ src/main/java/com/mikitellurium/clockoverlay/ClientInit.java
@@ -4,7 +4,7 @@ import com.mikitellurium.clockoverlay.config.Configuration;
 import com.mikitellurium.clockoverlay.event.ModEvents;
 import net.fabricmc.api.ClientModInitializer;
 
-public class Client implements ClientModInitializer {
+public class ClientInit implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
